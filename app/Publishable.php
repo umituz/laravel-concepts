250bb4598165de0d5f718dbe3d090cc2f15b4ce5
Ümit UZ <umituz998@gmail.com>
2020-03-07 22:56:36 +0300

    publish stubs

---
+++ app/Publishable.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Publishable
+ * @package App
+ */
+class Publishable extends Model
+{
+    protected $guarded = [];
+}
