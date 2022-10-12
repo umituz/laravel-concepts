d22e7ec697bc530597489b39c682fb094bf93bb3
umit.uz <umit.uz@beyn.com.tr>
2022-10-12 10:37:14 +0300

    .

--- general-laravel/app/Http/Composers/ChannelComposer.php
+++ general-laravel/app/Http/Composers/ChannelComposer.php
@@ -13,6 +13,6 @@ class ChannelComposer
 {
     public function compose(View $view)
     {
-        $view->with('channels', $channels = Channel::orderBy('name')->get());
+        $view->with('channels', Channel::orderBy('name')->get());
     }
 }
